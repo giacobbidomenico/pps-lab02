5ce4770000d2c2ef2d7a4f0148fe9304e10b62cf
Domenico Francesco Giacobbi <domenico.giacobbi@studio.unibo.it>
2026-04-17 23:36:04 +0200

    feat: implementation of point 8 of task3

--- src/main/scala/task3/Recursion.scala
+++ src/main/scala/task3/Recursion.scala
@@ -2,6 +2,8 @@ package task3
 
 object Recursion:
 
+  // 7
+
   def power(base: Double, exponent: Int): Double = exponent match
     case 0 => 1
     case _ => base * power(base, exponent - 1)
@@ -10,3 +12,15 @@ object Recursion:
   @main def testPower(): Unit =
     println(power(2, 3))
     println(power(5, 2))
+
+  // 8
+
+  def reverseNumber(n: Int): Int =
+    @annotation.tailrec
+    def _reverse(n: Int, acc: Int): Int = n match
+      case 0 => acc
+      case n => _reverse(n / 10, acc * 10 + n % 10)
+    _reverse(n, 0)
+
+  @main def testReverse(): Unit =
+    println(reverseNumber(5341))
--- src/test/scala/task3/TestRecursion.scala
+++ src/test/scala/task3/TestRecursion.scala
@@ -10,3 +10,6 @@ class TestRecursion:
     assertEquals(8.0, power(2, 3), 0.0)
     assertEquals(9.0, power(3, 2), 0.0)
     assertEquals(1.0, power(5, 0), 0.0)
+
+  @Test def testReverse(): Unit =
+    assertEquals(1435, reverseNumber(5341))
