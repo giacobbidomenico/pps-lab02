0837bc1c949947c0d8e54a69e5fa4e08537ece27
Domenico Francesco Giacobbi <domenico.giacobbi@studio.unibo.it>
2026-04-18 18:11:43 +0200

    feat: implementation of task5

--- src/main/scala/task5/Optionals.scala
+++ src/main/scala/task5/Optionals.scala
@@ -1,32 +1,42 @@
 package task5
 
-// overall module
 object Optionals:
 
-  // type "public" definition, exposing structure
-  enum OptionalInt:
-    case Just(value: Int)
+  enum Optional[+A]:
+    case Just(value: A)
     case Empty()
 
-  // operations (/algorithms)
-  object OptionalInt:
+  def isEmpty[A](opt: Optional[A]): Boolean = opt match
+    case Optional.Empty() => true
+    case _                => false
 
-    def isEmpty(opt: OptionalInt): Boolean = opt match
-      case Empty() => true
-      case _       => false
+  def orElse[A](opt: Optional[A], orElse: A): A = opt match
+    case Optional.Just(a) => a
+    case _                => orElse
 
-    def orElse(opt: OptionalInt, orElse: Int): Int = opt match
-      case Just(a) => a
-      case _       => orElse
+  def map[A, B](opt: Optional[A])(f: A => B): Optional[B] = opt match
+    case Optional.Just(a) => Optional.Just(f(a))
+    case _                => Optional.Empty()
+
+  def filter[A](opt: Optional[A])(p: A => Boolean): Optional[A] = opt match
+    case Optional.Just(a) if p(a) => Optional.Just(a)
+    case _                        => Optional.Empty()
 
 @main def tryOptionals(): Unit =
-  import Optionals.* // to work with Optionals (to see OptionalInt type)
-  import OptionalInt.* // to directly access algorithms
+  import Optionals.*
+  import Optional.*
 
-  val s1: OptionalInt = Just(1)
-  val s2: OptionalInt = Empty()
+  val s1: Optional[Int] = Just(1)
+  val s2: Optional[Int] = Empty()
 
-  println(s1) // Some(1)
-  println(isEmpty(s1)) // false
-  println(orElse(s1, 0)) // 1
-  println(orElse(s2, 0)) // 0
+  println(s1)
+  println(isEmpty(s1))
+  println(orElse(s1, 0))
+  println(orElse(s2, 0))
+
+  println(map(Just(5))(_ > 2)) // Just(true)
+  println(map(Empty[Int]())(_ > 2)) // Empty()
+
+  println(filter(Just(5))(_ > 2)) // Just(5)
+  println(filter(Just(5))(_ > 8)) // Empty()
+  println(filter(Empty[Int]())(_ > 2)) // Empty()
--- src/test/scala/task5/OptionalIntTest.scala
+++ src/test/scala/task5/OptionalIntTest.scala
@@ -3,22 +3,31 @@ package task5
 import org.junit.*
 import org.junit.Assert.*
 import Optionals.*
+import Optional.*
 
 class OptionalIntTest:
+
   @Test def emptyOptionalShouldBeEmpty(): Unit =
-    val empty = OptionalInt.Empty()
-    assertTrue(OptionalInt.isEmpty(empty))
+    val empty: Optional[Int] = Empty()
+    assertTrue(isEmpty(empty))
 
   @Test def nonEmptyOptionalShouldNotBeEmpty(): Unit =
-    val nonEmpty = OptionalInt.Just(0)
-    assertFalse(OptionalInt.isEmpty(nonEmpty))
+    val nonEmpty = Just(0)
+    assertFalse(isEmpty(nonEmpty))
 
   @Test def orElseShouldReturnDefaultWhenEmpty(): Unit =
-    val nonEmpty = OptionalInt.Just(0)
-    assertEquals(0, OptionalInt.orElse(nonEmpty, 1))
+    val empty: Optional[Int] = Empty()
+    assertEquals(1, orElse(empty, 1))
 
   @Test def orElseShouldReturnValueWhenNonEmpty(): Unit =
-    val empty = OptionalInt.Empty()
-    assertEquals(1, OptionalInt.orElse(empty, 1))
+    val nonEmpty = Just(0)
+    assertEquals(0, orElse(nonEmpty, 1))
 
-  /** Task 5: do test for map **/
+  @Test def testMap(): Unit =
+    assertEquals(Just(true), map(Just(5))(_ > 2))
+    assertEquals(Empty(), map(Empty[Int]())(_ > 2))
+
+  @Test def testFilter(): Unit =
+    assertEquals(Just(5), filter(Just(5))(_ > 2))
+    assertEquals(Empty(), filter(Just(5))(_ > 8))
+    assertEquals(Empty(), filter(Empty[Int]())(_ > 2))
